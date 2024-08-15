366ce8ccad9df841afde62d0f26a3a497357829a
wasif12334 <kashifaliraza95@gmail.com>
2024-08-15 22:37:38 +0500

    Added bulb and butn but my code have error that not woking

---
+++ script.js
@@ -0,0 +1,6 @@
+var btn = document.getElementsByClassName("bulb");
+var bulb = document.getElementsByClassName("center");
+//btn.addEventListener('click',on);
+function on(){
+    bulb.src= "BulbOn.png";
+}
